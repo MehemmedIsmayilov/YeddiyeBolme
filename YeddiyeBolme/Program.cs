9c29820897b5a166c6a2db1c92434710bc0623d1
MehemmedIsmayilov <mehemmedmi@code.edu.az>
2022-11-22 00:52:05 +0400

    Yeddiye Bolme Araligi Metodu

---
+++ YeddiyeBolme/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+int num = Convert.ToInt32(Console.ReadLine());
+int cavab;
+cavab = num / 7;
+int tamEded = num / 7 * 7;
+int tamEded2 = tamEded + 7; ;
+
+
+if (num % 7==0)
+{
+    
+    Console.Write("bolunur: ");
+    Console.WriteLine(cavab);
+    
+}
+else if (num % 7!=0)
+    {
+      if (num<tamEded+4)
+    {
+        Console.WriteLine(tamEded);
+    }
+    else
+    {
+    Console.WriteLine(tamEded2);
+
+    }
+    }
